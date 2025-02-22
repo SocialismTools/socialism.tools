9508e8794c1c1da3e89fec6c780ec71439af1a0c
Ian <admin@socialism.tools>
2025-02-21 22:42:18 -0600

    I hate git so much

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -45,7 +45,13 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      filterFn: (node) => {
+        // set containing names of everything you want to filter out
+        const omit = new Set(["tags", "glossary"])
+        return !omit.has(node.name.toLowerCase())
+      },
+    })),
   ],
   right: [],
 }
