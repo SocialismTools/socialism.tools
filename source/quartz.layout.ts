b72e11a97e8786c0fa4aad0d2a729e595071da73
Admin <admin@socialism.tools>
2025-11-20 15:28:59 -0600

    feat: Remove Spacer and Search components from the left sidebar and set mobile sidebar flex direction to column.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -19,8 +19,6 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
     Component.Darkmode(),
     Component.Explorer({
       title: "Content",
@@ -54,8 +52,6 @@ export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
     Component.Darkmode(),
     Component.Explorer({
       filterFn: (node) => {
